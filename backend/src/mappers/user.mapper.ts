2d96f19ce3ab2529422405c443cf2f3a674dd268
athulkrishnama <athulkrishnama24@gmail.com>
2025-10-02 17:02:56 +0530

    mappers to conerty create user dto to string for saving in cache and converting string to entity implemented

--- backend/src/mappers/user.mapper.ts
+++ backend/src/mappers/user.mapper.ts
@@ -1,5 +1,7 @@
 import { IUserDocument } from "@infrastructure/repository/database configs/schemas/userSchema";
 import { UserEntity } from "@domain/entities/user/user.entity";
+import { ICreateUserRequestDTO } from "@domain/dtos/auth/createUser.dto";
+import { Errors } from "./Errors";
 
 export class UserMapper {
   static toEntityfromMongooseDocument(document: IUserDocument): UserEntity {
@@ -16,6 +18,25 @@ export class UserMapper {
       travelerDetails: document.travelerDetails,
       createdAt: document.createdAt,
       lastLogin: document.lastLogin,
+    };
+  }
+
+  static toStringfromCreateUserDTO(dto: ICreateUserRequestDTO): string {
+    return JSON.stringify(dto);
+  }
+
+  static toEntityFromDTO(cachedData: string): UserEntity {
+    const { email, full_name, role, password } = JSON.parse(cachedData);
+    if (!(email && full_name && role && password)) {
+      throw new Error(Errors.REDIS_DATA_MISSING_ERROR);
+    }
+
+    return {
+      email,
+      full_name,
+      role,
+      password,
+      is_blocked: false,
     };
   }
 }
