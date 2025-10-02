91a2ec62af414fcc812562044756168d4028c910
athulkrishnama <athulkrishnama24@gmail.com>
2025-10-02 16:33:44 +0530

    add environment variable validations for tokens, email, and Redis URL

--- backend/src/infrastructure/validationSchemas/envValidation.ts
+++ backend/src/infrastructure/validationSchemas/envValidation.ts
@@ -13,4 +13,15 @@ export const envSchema = z.object({
     .enum(environment, { error: Errors.ENV_NODE_ENV_ERROR })
     .default("DEVELOPMENT"),
   ORIGIN_URL: z.url(),
+  ACCESS_TOKEN_SECRET: z.string({ error: Errors.ENV_ACCESS_TOKEN_ERROR }),
+  REFRESH_TOKEN_SECRET: z.string({ error: Errors.ENV_REFRESH_TOKEN_ERROR }),
+  ACCESS_TOKEN_EXPIRATION_TIME: z.coerce.number({
+    error: Errors.ENV_ACCESS_TOKEN_EXPIRATION_TIME_ERROR,
+  }),
+  REFRESH_TOKEN_EXPIRATION_TIME: z.coerce.number({
+    error: Errors.ENV_REFRESH_TOKEN_EXPIRATION_TIME_ERROR,
+  }),
+  EMAIL: z.email({ error: Errors.ENV_EMAIL_ERROR }),
+  EMAIL_PASSWORD: z.string({ error: Errors.ENV_EMAIL_PASSWORD_ERROR }),
+  REDIS_URL: z.url({ error: Errors.ENV_REDIS_URL_ERROR }),
 });
