9c9da2ad393dd7133f3b9d8dd17c236cb3c0d2bf
athulkrishnama <athulkrishnama24@gmail.com>
2025-10-02 18:14:17 +0530

    added verify otp usecase

--- backend/src/DI/useCases.ts
+++ backend/src/DI/useCases.ts
@@ -1,7 +1,13 @@
 import { ISignupUseCase } from "@domain/interfaces/useCase/auth/signupUseCase.interface";
+import { IVerifyOtpUseCase } from "@domain/interfaces/useCase/auth/verifyOtpUseCase.interface";
 import { SignupUseCase } from "@useCases/auth/signupUseCase";
+import { SignupVerifyOtpUseCase } from "@useCases/auth/signupVerifyOtpUseCase";
 import { container } from "tsyringe";
 
 export function registerUsecases() {
   container.registerSingleton<ISignupUseCase>("ISignupUseCase", SignupUseCase);
+  container.registerSingleton<IVerifyOtpUseCase>(
+    "IVerifyOtpUseCase",
+    SignupVerifyOtpUseCase,
+  );
 }
