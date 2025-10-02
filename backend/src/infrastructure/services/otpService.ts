6962f6e296f6939e42beadfc28bf20d27c2867f8
athulkrishnama <athulkrishnama24@gmail.com>
2025-10-02 16:31:56 +0530

    otp service implemented

---
+++ backend/src/infrastructure/services/otpService.ts
@@ -0,0 +1,13 @@
+import { IOtpService } from "@domain/interfaces/service/otpService.interface";
+import { injectable } from "tsyringe";
+
+@injectable()
+export class OtpService implements IOtpService {
+  generateOtp(digits: number): string {
+    return (
+      Math.pow(10, digits - 1) +
+      Math.floor(Math.random() * 9 * Math.pow(10, digits - 1)) +
+      ""
+    );
+  }
+}
