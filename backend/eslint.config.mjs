48bcf222f4368867d96d1726b58ac5a8809db989
athulkrishnama <athulkrishnama24@gmail.com>
2025-10-02 16:22:30 +0530

    ignored dist folder from linting

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 import tseslint from "typescript-eslint";
 import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
 
 export default defineConfig([
   ...tseslint.configs.recommended,
   eslintPluginPrettier,
-]
-);
+  globalIgnores(["./dist/*"]),
+]);
