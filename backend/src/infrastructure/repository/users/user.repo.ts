42c849675e3c69997bc5af0830a4ed3be7992a45
athulkrishnama <athulkrishnama24@gmail.com>
2025-10-02 16:30:50 +0530

    user repo implemented

---
+++ backend/src/infrastructure/repository/users/user.repo.ts
@@ -0,0 +1,24 @@
+import { IUserRepo } from "@domain/interfaces/repository/users/user.repo.interface.";
+import { BaseRepository } from "../base/base.repo";
+import { Model } from "mongoose";
+import { UserMapper } from "@mappers/user.mapper";
+import { UserEntity } from "@domain/entities/user/user.entity";
+import { inject, injectable } from "tsyringe";
+import { IUserDocument } from "../database configs/schemas/userSchema";
+
+@injectable()
+export class UserRepository
+  extends BaseRepository<UserEntity, IUserDocument>
+  implements IUserRepo
+{
+  constructor(@inject("IUserModel") protected _model: Model<IUserDocument>) {
+    super(_model);
+  }
+
+  async findByEmail(email: string): Promise<UserEntity | null> {
+    const user = await this._model.findOne({ email });
+
+    if (user) return UserMapper.toEntityfromMongooseDocument(user);
+    return null;
+  }
+}
