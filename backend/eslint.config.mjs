c340aeb882b802cdb1bf41d14d34da3f81b62bc6
athulkrishnama <athulkrishnama24@gmail.com>
2025-10-02 16:17:39 +0530

    fix in eslint

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,13 +1,11 @@
 // @ts-check
 
-import { defineConfig } from 'eslint/config';
-import tseslint from 'typescript-eslint';
-import eslintPluginPrettier from "eslint-plugin-prettier/recommended"
-
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
 
 export default defineConfig([
-
-    ...tseslint.configs.recommended,
-    eslintPluginPrettier
+  ...tseslint.configs.recommended,
+  eslintPluginPrettier,
 ]
-)
+);
