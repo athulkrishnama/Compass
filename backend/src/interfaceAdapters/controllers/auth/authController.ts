8bf15b6154ea4cf102f3eaf1f7983901155abd44
athulkrishnama <athulkrishnama24@gmail.com>
2025-10-02 19:14:39 +0530

    resend otp controller added

--- backend/src/interfaceAdapters/controllers/auth/authController.ts
+++ backend/src/interfaceAdapters/controllers/auth/authController.ts
@@ -1,7 +1,9 @@
 import { HTTP_STATUS_CODE } from "@domain/constants/statusCodes";
+import { ISignupResendOtpUsecase } from "@domain/interfaces/useCase/auth/signupResendOtpUseCase.interface";
 import { ISignupUseCase } from "@domain/interfaces/useCase/auth/signupUseCase.interface";
 import { IVerifyOtpUseCase } from "@domain/interfaces/useCase/auth/verifyOtpUseCase.interface";
 import {
+  emailValidationSchema,
   userRegistrationSchema,
   userRegistrationVerifyOtpSchema,
 } from "@infrastructure/validationSchemas/authValidation";
@@ -15,6 +17,8 @@ export class AuthController {
   constructor(
     @inject("ISignupUseCase") private _signupUseCase: ISignupUseCase,
     @inject("IVerifyOtpUseCase") private _verifyOtpUseCase: IVerifyOtpUseCase,
+    @inject("ISignupResendOtpUsecase")
+    private _resendOtpUseCase: ISignupResendOtpUsecase,
   ) {}
 
   async handleUserRegistration(
@@ -59,6 +63,31 @@ export class AuthController {
       );
 
       res.status(HTTP_STATUS_CODE.CREATED).json(response);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async handleUserRegistrationResendOtp(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) {
+    try {
+      const data = emailValidationSchema.safeParse(req.body.email);
+
+      if (data.error) {
+        throw new Error(data.error.issues[0].message);
+      }
+
+      await this._resendOtpUseCase.resend(data.data);
+
+      const response = HTTPResponseBuilder.buildSuccessResponse(
+        HTTP_STATUS_CODE.OK,
+        HttpResponseMessages.OTP_RESEND_SUCCESSFULLY,
+      );
+
+      res.status(HTTP_STATUS_CODE.OK).json(response);
     } catch (error) {
       next(error);
     }
