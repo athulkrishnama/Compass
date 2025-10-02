01dc2bf1a43fd00087f2b87943a999faf7591308
athulkrishnama <athulkrishnama24@gmail.com>
2025-10-02 18:22:16 +0530

    verify otp route added

--- backend/src/interfaceAdapters/controllers/auth/authController.ts
+++ backend/src/interfaceAdapters/controllers/auth/authController.ts
@@ -1,6 +1,10 @@
 import { HTTP_STATUS_CODE } from "@domain/constants/statusCodes";
 import { ISignupUseCase } from "@domain/interfaces/useCase/auth/signupUseCase.interface";
-import { userRegistrationSchema } from "@infrastructure/validationSchemas/authValidation";
+import { IVerifyOtpUseCase } from "@domain/interfaces/useCase/auth/verifyOtpUseCase.interface";
+import {
+  userRegistrationSchema,
+  userRegistrationVerifyOtpSchema,
+} from "@infrastructure/validationSchemas/authValidation";
 import { HttpResponseMessages } from "@interfaceAdapters/constants/httpResponseMessages";
 import { HTTPResponseBuilder } from "@utils/httpResponseBuilder";
 import { NextFunction, Request, Response } from "express";
@@ -10,6 +14,7 @@ import { inject, injectable } from "tsyringe";
 export class AuthController {
   constructor(
     @inject("ISignupUseCase") private _signupUseCase: ISignupUseCase,
+    @inject("IVerifyOtpUseCase") private _verifyOtpUseCase: IVerifyOtpUseCase,
   ) {}
 
   async handleUserRegistration(
@@ -30,6 +35,30 @@ export class AuthController {
       );
 
       res.status(HTTP_STATUS_CODE.OK).json(response);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async handleUserRegistrationVerifyOtp(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) {
+    try {
+      const data = userRegistrationVerifyOtpSchema.safeParse(req.body);
+
+      if (data.error) {
+        throw new Error(data.error.issues[0].message);
+      }
+      await this._verifyOtpUseCase.verify(data.data);
+
+      const response = HTTPResponseBuilder.buildSuccessResponse(
+        HTTP_STATUS_CODE.CREATED,
+        HttpResponseMessages.USER_SIGNUP_SUCCESS,
+      );
+
+      res.status(HTTP_STATUS_CODE.CREATED).json(response);
     } catch (error) {
       next(error);
     }
