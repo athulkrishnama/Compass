38f3e581de629845aa7627deae4170c61bb639b5
athulkrishnama <athulkrishnama24@gmail.com>
2025-10-02 18:23:21 +0530

    signup errors added

--- backend/src/useCases/constants/Errors.ts
+++ backend/src/useCases/constants/Errors.ts
@@ -1,3 +1,6 @@
 export enum AuthError {
   AUTH_EXISTING_EMAIL_ERROR = "User existing with same email",
+  OTP_EXPIRED_OR_NOT_REQUESTED = "OTP is expired or otp not requested",
+  INVALID_OTP = "OTP is invalid",
+  USER_DATA_MISSIING_IN_CACHE = "User data is missing from cache",
 }
