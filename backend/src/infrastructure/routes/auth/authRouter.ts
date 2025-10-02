dabc70d10fd15d16d26044ccdf25944c7d1a2d61
athulkrishnama <athulkrishnama24@gmail.com>
2025-10-02 16:31:08 +0530

    auth router created

---
+++ backend/src/infrastructure/routes/auth/authRouter.ts
@@ -0,0 +1,25 @@
+import { AuthRoutes } from "@infrastructure/constants/routes/authRoutes";
+import { authController } from "@DI/resolve";
+import { NextFunction, Request, Response, Router } from "express";
+
+export class AuthRouter {
+  private _router: Router;
+
+  constructor() {
+    this._router = Router();
+    this._setRoute();
+  }
+
+  private _setRoute() {
+    this._router.post(
+      AuthRoutes.REGISTER,
+      (req: Request, res: Response, next: NextFunction) => {
+        authController.handleUserRegistration(req, res, next);
+      },
+    );
+  }
+
+  public get_router() {
+    return this._router;
+  }
+}
