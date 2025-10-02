aab9080634231fb9efb7f65c101816a8b58dc74f
athulkrishnama <athulkrishnama24@gmail.com>
2025-10-02 16:34:31 +0530

    signup usecase implemented

---
+++ backend/src/useCases/auth/signupUseCase.ts
@@ -0,0 +1,45 @@
+import { ICreateUserRequestDTO } from "@domain/dtos/auth/createUser.dto";
+import { IUserRepo } from "@domain/interfaces/repository/users/user.repo.interface.";
+import { ICacheService } from "@domain/interfaces/service/cacheService.interface";
+import { IEmailService } from "@domain/interfaces/service/emailService.interface";
+import { IEmailTemplateGenerator } from "@domain/interfaces/service/emailTemplateGenerator.interface";
+import { IOtpService } from "@domain/interfaces/service/otpService.interface";
+import { ISignupUseCase } from "@domain/interfaces/useCase/auth/signupUseCase.interface";
+import { EmailPayloadType } from "@domain/types/emailPayload";
+import { EmailSubjects } from "@infrastructure/constants/emailConstants";
+import { AuthError } from "@useCases/constants/Errors";
+import { inject, injectable } from "tsyringe";
+
+@injectable()
+export class SignupUseCase implements ISignupUseCase {
+  constructor(
+    @inject("IUserRepo") private _userRepo: IUserRepo,
+    @inject("IOtpService") private _otpService: IOtpService,
+    @inject("IEmailService") private _emailService: IEmailService,
+    @inject("ICacheService") private _cacheService: ICacheService,
+    @inject("IOtpMailService")
+    private _emailTemplateGenerator: IEmailTemplateGenerator,
+  ) {}
+
+  async signup(userData: ICreateUserRequestDTO): Promise<void> {
+    const existingUser = await this._userRepo.findByEmail(userData.email);
+
+    if (existingUser) {
+      throw new Error(AuthError.AUTH_EXISTING_EMAIL_ERROR);
+    }
+
+    const OTP = this._otpService.generateOtp(6);
+
+    const htmlContent = this._emailTemplateGenerator.generateHtml({ otp: OTP });
+
+    const emailPayload: EmailPayloadType = {
+      receiverMailid: userData.email,
+      subject: EmailSubjects.REGISTRATION_SEND_OTP,
+      content: htmlContent,
+    };
+
+    await this._emailService.sendMail(emailPayload);
+
+    this._cacheService.setWithExpiry(`user:${userData.email}`, OTP, 60 * 5);
+  }
+}
