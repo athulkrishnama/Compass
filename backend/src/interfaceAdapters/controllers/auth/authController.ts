41c76025dc15c1b1c5420f76915c1fae4eadbda8
athulkrishnama <athulkrishnama24@gmail.com>
2025-10-03 17:53:58 +0530

    controllers for forget password added

--- backend/src/interfaceAdapters/controllers/auth/authController.ts
+++ backend/src/interfaceAdapters/controllers/auth/authController.ts
@@ -1,12 +1,17 @@
 import { env } from "@config/envConfig";
 import { HTTP_STATUS_CODE } from "@domain/constants/statusCodes";
 import { IJwtService } from "@domain/interfaces/service/jwtService.interface";
+import { IForgetPasswordResetPasswordUseCase } from "@domain/interfaces/useCase/auth/forgetPasswordResetPassword.interface";
+import { IForgetPasswordSendOtpUseCase } from "@domain/interfaces/useCase/auth/forgetPasswordSendOtpUseCase.interface";
+import { IForgetPasswordVerifyOtpUseCase } from "@domain/interfaces/useCase/auth/forgetPasswordVerifyOtpUseCase.interface";
 import { ILoginUseCase } from "@domain/interfaces/useCase/auth/loginUseCase.interface";
 import { ISignupResendOtpUsecase } from "@domain/interfaces/useCase/auth/signupResendOtpUseCase.interface";
 import { ISignupUseCase } from "@domain/interfaces/useCase/auth/signupUseCase.interface";
 import { IVerifyOtpUseCase } from "@domain/interfaces/useCase/auth/verifyOtpUseCase.interface";
 import {
   emailValidationSchema,
+  forgetPasswordResetPasswordSchema,
+  forgetPasswordVerifyOtpSchema,
   loginValidationSchema,
   userRegistrationSchema,
   userRegistrationVerifyOtpSchema,
@@ -26,6 +31,12 @@ export class AuthController {
     private _resendOtpUseCase: ISignupResendOtpUsecase,
     @inject("ILoginUseCase") private _loginUseCase: ILoginUseCase,
     @inject("IJwtService") private _jwtService: IJwtService,
+    @inject("IForgetPasswordSendOtpUseCase")
+    private _forgetPasswordSendOtpUseCase: IForgetPasswordSendOtpUseCase,
+    @inject("IForgetPasswordVerifyOtpUseCase")
+    private _forgetPasswordVerifyOtpUseCase: IForgetPasswordVerifyOtpUseCase,
+    @inject("IForgetPasswordResetPasswordUseCase")
+    private _forgetPassawordResetPasswordUseCase: IForgetPasswordResetPasswordUseCase,
   ) {}
 
   async handleUserRegistration(
@@ -130,6 +141,83 @@ export class AuthController {
         HTTP_STATUS_CODE.OK,
         HttpResponseMessages.USER_LOGIN_SUCCESSFULL,
         { userData: responseDto, accessToken },
+      );
+
+      res.status(HTTP_STATUS_CODE.OK).json(response);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async handleForgetPasswordSendOtp(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) {
+    try {
+      const data = emailValidationSchema.safeParse(req.body.email);
+
+      if (data.error) {
+        throw new Error(data.error.issues[0].message);
+      }
+
+      await this._forgetPasswordSendOtpUseCase.sendOtp(data.data);
+
+      const response = HTTPResponseBuilder.buildSuccessResponse(
+        HTTP_STATUS_CODE.OK,
+        HttpResponseMessages.OTP_RESEND_SUCCESSFULLY,
+      );
+      res.status(HTTP_STATUS_CODE.OK).json(response);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async handleForgetPasswordVerifyOtp(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) {
+    try {
+      const data = forgetPasswordVerifyOtpSchema.safeParse(req.body);
+
+      if (data.error) {
+        throw new Error(data.error.issues[0].message);
+      }
+
+      const token = await this._forgetPasswordVerifyOtpUseCase.verify(
+        data.data,
+      );
+
+      const response = HTTPResponseBuilder.buildSuccessResponse(
+        HTTP_STATUS_CODE.OK,
+        HttpResponseMessages.OTP_VERIFIED_SUCCESSFULLY,
+        { token },
+      );
+
+      res.status(HTTP_STATUS_CODE.OK).json(response);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async handleForgetpasswordPasswordReset(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) {
+    try {
+      const data = forgetPasswordResetPasswordSchema.safeParse(req.body);
+
+      if (data.error) {
+        throw new Error(data.error.issues[0].message);
+      }
+
+      await this._forgetPassawordResetPasswordUseCase.reset(data.data);
+
+      const response = HTTPResponseBuilder.buildSuccessResponse(
+        HTTP_STATUS_CODE.OK,
+        HttpResponseMessages.PASSWORD_RESET_SUCCESSFUL,
       );
 
       res.status(HTTP_STATUS_CODE.OK).json(response);
