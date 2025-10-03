3601602d3d7f854c4058d85e3e4f1fddfdc7483c
athulkrishnama <athulkrishnama24@gmail.com>
2025-10-03 12:04:14 +0530

    errors and messages for login added

--- backend/src/interfaceAdapters/constants/httpResponseMessages.ts
+++ backend/src/interfaceAdapters/constants/httpResponseMessages.ts
@@ -2,4 +2,5 @@ export enum HttpResponseMessages {
   OTP_SEND_SUCCESSFULLY = "Otp send successfully",
   USER_SIGNUP_SUCCESS = "User signup completed successfully",
   OTP_RESEND_SUCCESSFULLY = "OTP successfully resended",
+  USER_LOGIN_SUCCESSFULL = "User login successful",
 }
--- backend/src/useCases/constants/Errors.ts
+++ backend/src/useCases/constants/Errors.ts
@@ -3,4 +3,7 @@ export enum AuthError {
   OTP_EXPIRED_OR_NOT_REQUESTED = "OTP is expired or otp not requested",
   INVALID_OTP = "OTP is invalid",
   USER_DATA_MISSIING_IN_CACHE = "User data is missing from cache",
+  USER_NOT_FOUND = "User not found with this email",
+  USER_IS_BLOCKED = "User is blocked",
+  PASSWORD_NOT_MATCHING = "Password not matching",
 }
