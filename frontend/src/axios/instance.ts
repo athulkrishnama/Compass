60b7127725c7fbddecded19e0ca76eebe52ea1fc
athulkrishnama <athulkrishnama24@gmail.com>
2025-10-06 20:30:15 +0530

    intercepters added

--- frontend/src/axios/instance.ts
+++ frontend/src/axios/instance.ts
@@ -1,8 +1,39 @@
+import { AUTH_ROUTES } from "@/constants/routes/authRoutes";
+import { store } from "@/store/store";
 import axios from "axios";
 
 export const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_BASEURL,
-    withCredentials: true
+    withCredentials: true,
 });
 
+axiosInstance.interceptors.request.use(function (config) {
+    if (store.getState().token.accessToken) {
+        config.headers.Authorization =
+            "Bearer " + store.getState().token.accessToken;
+    }
+    return config;
+});
 
+axiosInstance.interceptors.response.use(
+    (res) => res,
+    async (err) => {
+        const originalRequest = err.config;
+
+        if (
+            err.response.status === 401 &&
+            err.response.data.error === "Invalid Token" &&
+            !originalRequest.retry
+        ) {
+            try {
+                originalRequest.retry = true;
+                const response = await axiosInstance.post(AUTH_ROUTES.REFRESH);
+                originalRequest.headers.Authorization =
+                    "Bearer " + response.data.data.accessToken;
+                return axiosInstance(originalRequest);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }
+);
