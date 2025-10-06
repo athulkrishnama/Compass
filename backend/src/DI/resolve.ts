a4efe45880f5297a0f4fd6d65cd7c821cfa94350
athulkrishnama <athulkrishnama24@gmail.com>
2025-10-06 20:26:42 +0530

    DI for auth middleware added

--- backend/src/DI/resolve.ts
+++ backend/src/DI/resolve.ts
@@ -1,6 +1,8 @@
 import { AuthController } from "@interfaceAdapters/controllers/auth/authController";
 import { container } from "tsyringe";
 import { registerDI } from "@DI/register";
+import { AuthMiddleware } from "@infrastructure/middlewares/authMiddleware";
 
 registerDI();
 export const authController = container.resolve(AuthController);
+export const authMiddleware = container.resolve(AuthMiddleware);
--- backend/src/DI/useCases.ts
+++ backend/src/DI/useCases.ts
@@ -2,6 +2,7 @@ import { IForgetPasswordResetPasswordUseCase } from "@domain/interfaces/useCase/
 import { IForgetPasswordSendOtpUseCase } from "@domain/interfaces/useCase/auth/forgetPasswordSendOtpUseCase.interface";
 import { IForgetPasswordVerifyOtpUseCase } from "@domain/interfaces/useCase/auth/forgetPasswordVerifyOtpUseCase.interface";
 import { ILoginUseCase } from "@domain/interfaces/useCase/auth/loginUseCase.interface";
+import { IRefreshTokenUseCase } from "@domain/interfaces/useCase/auth/refreshTokenUseCase.interface";
 import { ISignupResendOtpUsecase } from "@domain/interfaces/useCase/auth/signupResendOtpUseCase.interface";
 import { ISignupUseCase } from "@domain/interfaces/useCase/auth/signupUseCase.interface";
 import { IVerifyOtpUseCase } from "@domain/interfaces/useCase/auth/verifyOtpUseCase.interface";
@@ -9,6 +10,7 @@ import { ForgetPasswordResetPasswordUseCase } from "@useCases/auth/forgetPasswor
 import { ForgetPasswordSendOtpUseCase } from "@useCases/auth/forgetPasswordSendOtpUseCase";
 import { ForgetPasswordVerifyOtpUseCase } from "@useCases/auth/forgetPasswordVerifyOtpUseCase";
 import { LoginUseCase } from "@useCases/auth/loginUseCase";
+import { RefreshTokenUseCase } from "@useCases/auth/refreshTokenUseCase";
 import { SignupResendOtpUseCase } from "@useCases/auth/signupResendOtpUseCase";
 import { SignupUseCase } from "@useCases/auth/signupUseCase";
 import { SignupVerifyOtpUseCase } from "@useCases/auth/signupVerifyOtpUseCase";
@@ -36,5 +38,9 @@ export function registerUsecases() {
   container.registerSingleton<IForgetPasswordResetPasswordUseCase>(
     "IForgetPasswordResetPasswordUseCase",
     ForgetPasswordResetPasswordUseCase,
+  );
+  container.registerSingleton<IRefreshTokenUseCase>(
+    "IRefreshTokenUseCase",
+    RefreshTokenUseCase,
   );
 }
