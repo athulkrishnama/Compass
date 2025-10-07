aea5af041fdbaf2da276873c6345c6a0eec1a49c
athulkrishnama <athulkrishnama24@gmail.com>
2025-10-07 11:42:03 +0530

    added get users and status change methods

--- backend/src/infrastructure/repository/users/user.repo.ts
+++ backend/src/infrastructure/repository/users/user.repo.ts
@@ -1,10 +1,12 @@
 import { IUserRepo } from "@domain/interfaces/repository/users/user.repo.interface.";
 import { BaseRepository } from "../base/base.repo";
-import { Model } from "mongoose";
+import { Model, RootFilterQuery } from "mongoose";
 import { UserMapper } from "@mappers/user.mapper";
 import { UserEntity } from "@domain/entities/user/user.entity";
 import { inject, injectable } from "tsyringe";
 import { IUserDocument } from "../database configs/schemas/userSchema";
+import { ROLES } from "@domain/types/roles";
+import { VALUES } from "@infrastructure/constants/values";
 
 @injectable()
 export class UserRepository
@@ -27,5 +29,54 @@ export class UserRepository
     password: string,
   ): Promise<void> {
     await this._model.updateOne({ email }, { $set: { password } });
+  }
+
+  async getUsersWithFilter(
+    pageNo: number,
+    role: ROLES[] | undefined,
+    status: boolean | undefined,
+    query: string | undefined,
+  ): Promise<{ users: UserEntity[]; total: number }> {
+    const skip = (pageNo - 1) * VALUES.GET_USERS_LIMIT;
+
+    const filter: RootFilterQuery<IUserDocument> = {};
+
+    if (status !== undefined) {
+      filter.is_blocked = status;
+    }
+
+    if (query !== undefined) {
+      filter.query = query;
+    }
+
+    if (role) {
+      for (const r of role) {
+        if (filter.role) {
+          filter.role.push(r);
+        } else {
+          filter.role = [r];
+        }
+      }
+    }
+
+    console.log("filter", filter);
+    const response = await this._model
+      .find(filter)
+      .skip(skip)
+      .limit(VALUES.GET_USERS_LIMIT);
+
+    const totalDocuments = Math.ceil(
+      (await this._model.countDocuments(filter)) / VALUES.GET_USERS_LIMIT,
+    );
+
+    const users = response.map((user) =>
+      UserMapper.toEntityfromMongooseDocument(user),
+    );
+
+    return { users, total: totalDocuments };
+  }
+
+  async userStatusChange(id: string, status: boolean): Promise<void> {
+    await this._model.findByIdAndUpdate(id, { is_blocked: status });
   }
 }
