ad10361683ad24504358942518334b9aad66a447
athulkrishnama <athulkrishnama24@gmail.com>
2025-10-07 15:44:31 +0530

    token invalidation usecase implemented

---
+++ backend/src/useCases/auth/tokenInvalidationUseCase.ts
@@ -0,0 +1,27 @@
+import { env } from "@config/envConfig";
+import { ICacheService } from "@domain/interfaces/service/cacheService.interface";
+import { IJwtService } from "@domain/interfaces/service/jwtService.interface";
+import { ITokenInvalidationUseCase } from "@domain/interfaces/useCase/auth/tokenInvalidationUseCase.interface";
+import { inject, injectable } from "tsyringe";
+
+@injectable()
+export class TokenInvalidationUseCase implements ITokenInvalidationUseCase {
+  constructor(
+    @inject("ICacheService") private _cacheService: ICacheService,
+    @inject("IJwtService") private _jwtService: IJwtService,
+  ) {}
+
+  async validate(token: string): Promise<void> {
+    const decoded = this._jwtService.verifyAccessToken(token);
+
+    if (!decoded) return;
+
+    const tokenId = decoded.jti;
+
+    await this._cacheService.setWithExpiry(
+      `blackList:${tokenId}`,
+      "blackListed",
+      env.ACCESS_TOKEN_EXPIRATION_TIME,
+    );
+  }
+}
