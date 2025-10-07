d5ef661d234ec4077ab8d824fd2a0ede49511219
athulkrishnama <athulkrishnama24@gmail.com>
2025-10-07 11:44:39 +0530

    axios bug fixed

--- frontend/src/axios/instance.ts
+++ frontend/src/axios/instance.ts
@@ -34,6 +34,8 @@ axiosInstance.interceptors.response.use(
             } catch (error) {
                 console.log(error);
             }
+        }else{
+            return Promise.reject(err)
         }
     }
 );
