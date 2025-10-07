bed99e33138137d496a980ac89e5e7ad801833b3
athulkrishnama <athulkrishnama24@gmail.com>
2025-10-07 11:46:02 +0530

    layout updated with sidebar

--- frontend/src/routes/admin/route.tsx
+++ frontend/src/routes/admin/route.tsx
@@ -1,13 +1,22 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router";
+import SideBar from "@/components/admin/SideBar";
+import { createFileRoute, Outlet, useLocation } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/admin")({
     component: RouteComponent,
 });
 
 function RouteComponent() {
-    return (
-        <div className="h-full w-full">
-            <Outlet />
+    const location = useLocation();
+    console.log(location.pathname);
+    return location.pathname === "/admin/login" ? (
+        <Outlet />
+    ) : (
+        <div className="flex h-screen w-full">
+            <SideBar />
+
+            <div className="flex-grow bg-gray-50 p-6 overflow-y-auto">
+                <Outlet />
+            </div>
         </div>
     );
 }
