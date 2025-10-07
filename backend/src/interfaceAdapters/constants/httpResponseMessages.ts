870b5ca1dc3e73b6654bdea1e4a1efb1fef92f82
athulkrishnama <athulkrishnama24@gmail.com>
2025-10-07 11:44:21 +0530

    validations and constants added

--- backend/src/interfaceAdapters/constants/httpResponseMessages.ts
+++ backend/src/interfaceAdapters/constants/httpResponseMessages.ts
@@ -6,4 +6,6 @@ export enum HttpResponseMessages {
   OTP_VERIFIED_SUCCESSFULLY = "OTP verified Successfully",
   PASSWORD_RESET_SUCCESSFUL = "Password reset is successful",
   REFRESH_SUCCESSFUL = "Successfuly got new access token",
+  DATA_FETCHED_SUCCESSFULLY = "Data fetched successfully",
+  STATUS_UPDATED_SUCCESSFULLY = "User Status updated Successfully",
 }
