0e358b011b88694ae524f7ce6b30f80249021794
athulkrishnama <athulkrishnama24@gmail.com>
2025-12-01 20:13:50 +0530

    Implement route protection for traveler routes based on user authentication and role

--- frontend/src/routes/traveler/route.tsx
+++ frontend/src/routes/traveler/route.tsx
@@ -1,9 +1,32 @@
 import Navbar from "@/components/shared/Navbar/Navbar";
+import { ROLES } from "@/constants/roles";
+import type { FileRoutesByTo } from "@/routeTree.gen";
 import translationKey from "@/utils/i18n/translationKey";
-import { createFileRoute, Outlet, useLocation } from "@tanstack/react-router";
+import {
+    createFileRoute,
+    Outlet,
+    redirect,
+    useLocation,
+} from "@tanstack/react-router";
 import { useTranslation } from "react-i18next";
 export const Route = createFileRoute("/traveler")({
     component: RouteComponent,
+    beforeLoad: ({ context, location }) => {
+        const allowedRoutes: (keyof FileRoutesByTo)[] = [
+            "/traveler/login",
+            "/traveler/signup",
+            "/traveler/forgetPassword",
+        ];
+        if (
+            !context.isLoggedin() ||
+            (!context.checkRole(ROLES.TRAVELER) &&
+                !allowedRoutes.includes(
+                    location.pathname as keyof FileRoutesByTo
+                ))
+        ) {
+            throw redirect({ to: "/traveler/login", replace: true });
+        }
+    },
 });
 
 function RouteComponent() {
