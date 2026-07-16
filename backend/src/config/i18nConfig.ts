bf77cedd71f7776153b26b8681ffa872fb939999
athulkrishnama <athulkrishnama24@gmail.com>
2026-07-16 19:42:18 +0530

    fix: update build process to include locale files and adjust i18n path resolution for production environment

--- backend/src/config/i18nConfig.ts
+++ backend/src/config/i18nConfig.ts
@@ -2,13 +2,18 @@ import i18next from "i18next";
 import Backend from "i18next-fs-backend";
 import middleware from "i18next-http-middleware";
 import { join } from "path";
+import { env } from "./envConfig";
+
+const isProduction: boolean = env.NODE_ENV === "PRODUCTION";
 
 i18next
   .use(Backend)
   .use(middleware.LanguageDetector)
   .init({
     backend: {
-      loadPath: join(process.cwd(), "src/locales", "{{lng}}", "{{ns}}.json"),
+      loadPath: isProduction
+        ? join(process.cwd(), "dist", "locales", "{{lng}}", "{{ns}}.json")
+        : join(process.cwd(), "src", "locales", "{{lng}}", "{{ns}}.json"),
     },
     detection: {
       order: ["header"],
